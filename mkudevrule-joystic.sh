5b84040a44c5832d7745eac7f4a71c2840c71c62
R. Miloh Alexander <miloh@froggytoad.net>
2014-07-27 07:16:19 -0700

    add mkudevrule shell script that is hardcoded to an afterglow controller, generalize later

---
+++ mkudevrule-joystic.sh
@@ -0,0 +1,11 @@
+cat > /etc/udev/rules.d/50-even.rules <<EOF
+KERNEL=="js*", ATTRS{idVendor}=="0e6f", GROUP="dialout", MODE="660" 
+
+KERNEL=="input*", ACTION=="add", ATTRS{idVendor}=="0e6f", \
+	ATTR{name}=="Afterglow controller for PS3", RUN+="/usr/sbin/hold-octoprint.sh $env{ACTION}"
+
+ACTION=="remove", SUBSYSTEMS=="usb", ATTRS{idVendor}=="0e6f",  RUN+="/usr/sbin/hold-octoprint.sh $env{ACTION}"
+
+EOF
+
+sudo udevadm control --reload-rules
